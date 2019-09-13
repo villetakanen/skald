dd841053635ddab2acba939175de74049437f944
Ville Takanen <ville.takanen@foodie.fm>
2019-09-13 20:35:34 +0300

    Added the colors from skald-vue

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,13 +7,17 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
+        primary: '#039be5',
+        secondary: '#fbc02d',
         accent: '#82B1FF',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
         warning: '#FFC107'
+      },
+      dark: {
+        primary: '#455a64',
+        secondary: '#e91e63'
       }
     }
   },
---
+++ src/views/Stylebook.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <div class="style-book-chapter">
+      <p>This is a stylebook, with all elements we use shown, for UX design and verification</p>
+    </div>
+
+    <div class="style-book-chapter">
+      <v-btn>v-btn</v-btn>
+    </div>
+    <div class="style-book-chapter">
+      <v-btn color="primary">v-btn primary</v-btn>
+    </div>
+    <div class="style-book-chapter">
+      <v-btn color="secondary">v-btn secondary</v-btn>
+    </div>
+    <div class="style-book-chapter">
+      <v-btn dark>v-btn dark</v-btn>
+    </div>
+
+  </div>
+</template>
+<style scoped>
+div.style-book-chapter{
+  margin: 8px;
+  padding: 8px;
+}
+</style>
