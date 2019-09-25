48f9354aee555e1a4120065caeb0506407991acf
Ville Takanen <ville.takanen@foodie.fm>
2019-09-25 08:33:37 +0300

    -d

---
+++ src/store/site.js
@@ -0,0 +1,115 @@
+import Vue from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const state = {
+  siteid: null,
+  owners: {},
+  data: {},
+  unsubscibe: () => {},
+  unsubscibeOwners: () => {}
+}
+
+const getters = {
+  id: (context) => () => {
+    return context.siteid
+  },
+  owners: (context) => () => {
+    return context.owners
+  }
+}
+
+const mutations = {
+  site (context, { siteid, data }) {
+    Vue.set(context, 'siteid', siteid)
+    Vue.set(context, 'data', data)
+  },
+  owners (context, owners) {
+    if (!exists(owners)) owners = {}
+    Vue.set(context, 'owners', owners)
+  },
+  patchOwner (context, { id, data }) {
+    // Owners is reactive, so we need Vue to set it
+    var owners = context.owners
+    owners[id] = data
+    Vue.set(context, 'owners', owners)
+  }
+}
+
+const actions = {
+  /**
+   * Opens a site, and starts listening for changess
+   * @param {*} context Vuex context
+   * @param {*} siteid Firestore sites UUID
+   */
+  open (context, siteid) {
+    if (!exists(siteid)) throw new Error('invalid site id')
+    if (siteid === context.getters['id']()) {
+      // Nothing to do, as the site is open.
+      //
+      // Please note: this actually means, that we need to listen to changes
+      // for the open site, and not just get it once.
+      return
+    }
+
+    // unsubscribe from previous site
+    context.state.unsubscibe()
+    context.state.unsubscibeOwners()
+
+    // reset all patched data
+    context.commit('owners', null)
+
+    const db = firebase.firestore()
+
+    const siteRef = db.collection('sites').doc(siteid)
+    const ownersRef = siteRef.collection('owners')
+
+    // Subscribe to site data changes
+    context.state.unsubscribe = siteRef.onSnapshot((change) => {
+      context.commit('site', { siteid: siteid, data: change.data() })
+    })
+
+    // Subscribe to owner data changes
+    context.state.unsubscribeOwners = ownersRef.onSnapshot((change) => {
+      change.docChanges().forEach((owner) => {
+        context.commit('patchOwner', { id: owner.id, data: owner.data() })
+      })
+    })
+  },
+  /* getOwners (context) {
+    if (!exists(context.getters['id']())) throw new Error('A site is not opened, can not get owners')
+
+    const db = firebase.firestore()
+    const siteid = context.getters['id']()
+
+    const ownersRef = db.collection('sites').doc(siteid).collection('owners')
+    ownersRef.get().then((owners) => {
+      context.commit('owners', null)
+      var newOwners = {}
+      owners.forEach((owner) => {
+        newOwners[owner.id] = owner.data()
+      })
+      context.commit('owners', newOwners)
+    })
+  }, */
+  addOwner (context, { nick }) {
+    if (!exists(context.getters['id']())) return
+    console.log('adding owner ', nick)
+  },
+  removeOwner (context, { nick }) {
+    if (!exists(context.getters['id']())) return
+    console.log('adding owner ', nick)
+  }
+}
+
+function exists (value) {
+  if (value === null || typeof value === 'undefined') return false
+  return true
+}
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
