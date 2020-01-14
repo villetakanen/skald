327b3ad5e7d55a625f03b9a18314504c8d8e6a4d
Ville Takanen <ville.takanen@foodie.fm>
2020-01-14 15:09:46 +0200

    0.5.2

--- src/views/ListAttachments.vue
+++ src/views/ListAttachments.vue
@@ -8,15 +8,18 @@
             <Loading v-if="loading"/>
             <div v-if="!loading">
                <template v-for="(file, index) in files">
-                <p class="file" v-bind:key="index">
-                  {{file.name}}
-                  <span class="path">{{file.path}}</span>
-                  <v-btn
+                <div class="attachment" v-bind:key="index">
+                  <div class="preview">
+                    <img :src="file.path" :alt="file.name"/>
+                  </div>
+                  <p class="name">[wiki:<a :href="file.path">{{file.name}}</a>]</p>
+                  <div class="actions"><v-btn
                     x-small
                     class="ml-2"
                     outlined
                     @click="deleteFile(file.name)">Delete</v-btn>
-                  </p>
+                  </div>
+                  </div>
               </template>
             </div>
           </v-card-text>
@@ -59,10 +62,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-p.file{
+div.attachment{
   margin: 0;
-  padding: 0;
+  padding: 8px;
   line-height: 28px;
   font-size: 16px;
+  position: relative;
+  p{
+    margin: 0;
+    padding: 0;
+    margin-left: 44px;
+  }
+  div.actions{
+    position: absolute;
+    right: 16px;
+    top: 8px;
+  }
+  div.preview{
+    position: absolute;
+    left: 6px;
+    top: 6px;
+    border-radius: 50%;
+    width: 32px;
+    height: 32px;
+    background-color: rgba(#039be5, 0.7);
+    text-align: center;
+    img{
+      max-width: 100%;
+      max-height: 100%;
+      vertical-align: middle;
+      align-self: center;
+    }
+  }
+  a {
+    text-decoration: none;
+  }
+}
+div.attachment:hover{
+  background-color: rgba(0, 0, 0, 0.2)
+}
+div.attachment + div.attachment {
+  border-top: solid 1px #666;
 }
 </style>
