d0d63f9cb3dd38c822e08c828d06a4f6b94d2219
Ville Takanen <vtak@Villes-MacBook-Pro.local>
2020-09-01 10:52:10 +0300

    feat: create page nav action added

--- src/components/createpageaction/CreatePageAction.vue
+++ src/components/createpageaction/CreatePageAction.vue
@@ -3,13 +3,15 @@
     v-model="dialog"
     persistent max-width="570">
     <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        color="primary"
-        dark
-        v-bind="attrs"
+      <v-list-item
+        id="navi-add-page-action"
         v-on="on"
-        >{{ $t('actions.createPage') }}</v-btn>
-      </template>
+        v-bind="attrs"
+        @click="openDialog">
+        <v-list-item-action><v-icon>mdi-file-plus</v-icon></v-list-item-action>
+        <v-list-item-title>{{$t('actions.createPage')}}</v-list-item-title>
+      </v-list-item>
+    </template>
       <v-card>
         <v-card-title class="headline">Use Google's location service?</v-card-title>
         <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
@@ -21,3 +23,14 @@
       </v-card>
     </v-dialog>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup (props) {
+    const dialog = ref(false)
+    return { dialog }
+  }
+})
+</script>
