7226f1e1eba333d257e6c291c48a15a40e33812b
Ville Takanen <vtak@Villes-MacBook-Pro.local>
2020-09-14 13:50:10 +0300

    chore: addtags

--- src/components/playerlist/PlayerTagsDialog.vue
+++ src/components/playerlist/PlayerTagsDialog.vue
@@ -17,8 +17,25 @@
     </template>
 
     <v-card>
-      <v-card-title>{{$t('view_gm_players.addPlayerToSite')}}</v-card-title>
-      <v-card-text>{{$t('view_gm_players.addPlayerToSite')}}</v-card-text>
+      <v-card-title>{{player.nick}}</v-card-title>
+      <v-card-text>
+        <template v-for="(tag, index) in player.tags">
+          <v-chip
+            v-bind:key="index"
+            color="primary">{{tag}}</v-chip>
+        </template>
+        <hr/>
+        <template v-for="(tag, index2) in availableTags">
+          <v-chip
+            v-bind:key="index2"
+            color="dark"
+            @click="addTag(tag)">
+            <v-avatar left>
+              <v-icon>mdi-plus</v-icon>
+            </v-avatar>
+            {{tag}}</v-chip>
+        </template>
+      </v-card-text>
       <v-card-actions>{{$t('view_gm_players.addPlayerToSite')}}</v-card-actions>
     </v-card>
   </v-dialog>
@@ -29,6 +46,8 @@ import Vue from 'vue'
 import VueCompositionApi, { computed, defineComponent, onMounted, ref } from '@vue/composition-api'
 import { usePlayers } from '@/lib/usePlayers'
 import { useProfile } from '@/lib/useProfile'
+import firebase, { FirebaseError } from 'firebase/app'
+import 'firebase/firestore'
 Vue.use(VueCompositionApi)
 
 export default defineComponent({
@@ -43,11 +62,34 @@ export default defineComponent({
     }
   },
   setup (props) {
-    const { getPlayer } = usePlayers()
+    const { getPlayer, players } = usePlayers()
     const { isOwner } = useProfile()
     const player = computed(() => (getPlayer(props.uid)))
     const dialog = ref(false)
-    return { dialog, player, isOwner }
+    const availableTags = computed(() => {
+      const av:string[] = []
+      players.value.tags.forEach((tag) => {
+        if (!player.value || !player.value.tags) {
+          av.push(tag)
+        } else if (!player.value.tags.includes(tag)) {
+          av.push(tag)
+        }
+      })
+      return av
+    })
+    function addTag (tag:string):void {
+      // noop if exists
+      if (player.value && player.value.tags &&
+        player.value.tags.includes(tag)) return
+      const db = firebase.firestore()
+      const playerRef = db.collection('sites').doc(props.siteid).collection('players').doc(props.uid)
+      let newTags:string[]
+      if (player.value && player.value.tags) newTags = player.value.tags
+      else newTags = []
+      newTags.push(tag)
+      playerRef.update({ tags: newTags })
+    }
+    return { dialog, player, isOwner, availableTags, addTag }
   }
 })
 </script>
--- src/lib/usePlayers/index.ts
+++ src/lib/usePlayers/index.ts
@@ -10,7 +10,11 @@ export interface Player {
 }
 
 const playerListStruct:Player[] = []
-const players = ref({ list: playerListStruct })
+const playerTagsStruct:string[] = []
+const players = ref({
+  list: playerListStruct,
+  tags: playerTagsStruct
+})
 const meta = ref({
   activeSiteId: '',
   loading: false
@@ -18,11 +22,18 @@ const meta = ref({
 
 let unsubscribe = () => {}
 
+function pushTags (tags:string[]):void {
+  tags.forEach((element:string) => {
+    if (!players.value.tags.includes(element)) players.value.tags.push(element)
+  })
+}
+
 function subscribePlayers (siteid:string):void {
   if (meta.value.activeSiteId === siteid) return
   unsubscribe()
   meta.value.activeSiteId = siteid
   players.value.list.length = 0
+  players.value.tags.length = 0
   if (!siteid) return
   meta.value.loading = true
   const db = firebase.firestore()
@@ -34,6 +45,19 @@ function subscribePlayers (siteid:string):void {
           uid: change.doc.id,
           nick: change.doc.data().nick,
           tags: change.doc.data().tags
+        })
+        if (change.doc.data().tags) {
+          pushTags(change.doc.data().tags)
+        }
+      } else if (change.type === 'modified') {
+        players.value.list.forEach((p:Player) => {
+          if (p.uid === change.doc.id) {
+            p.nick = change.doc.data().nick
+            p.tags = change.doc.data().tags
+          }
+          if (change.doc.data().tags) {
+            pushTags(change.doc.data().tags)
+          }
         })
       } else if (change.type === 'removed') {
         const clearedList: Player[] = players.value.list.filter((item) => { return item.uid !== change.doc.id })
@@ -50,10 +74,11 @@ function subscribePlayers (siteid:string):void {
  * @param uid the UID of Player of the active site
  */
 function getPlayer (uid:string):Player|null {
+  let r = null
   players.value.list.forEach((p:Player) => {
-    if (p.uid === uid) return p
+    if (p.uid === uid) r = p
   })
-  return null
+  return r
 }
 
 export function usePlayers () {
